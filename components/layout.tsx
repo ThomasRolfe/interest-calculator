aa266b4748a82d1abf086806035657938402b5c3
Tom Rolfe <thomasrolfe@yahoo.co.uk>
2022-06-26 16:19:09 +0100

    Initial commit

---
+++ components/layout.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { FC } from 'react';
+
+const Layout = ({children}: any) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Layout;
---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Layout from '../components/layout';
+
+const About: NextPage = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>About page</h1> 
+            </div>
+        </Layout>
+    )
+}
+
+export default About;
